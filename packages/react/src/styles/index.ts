acd88c200198c5a3e53c3b7405c55bdeb3026565
João Vitor Lima <jvolima2004@gmail.com>
2022-10-21 11:12:13 -0300

    Configuração do Stitches

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,37 @@
+import {
+  colors,
+  fonts,
+  fontWeights,
+  fontSizes,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fonts,
+    fontWeights,
+    fontSizes,
+    lineHeights,
+    radii,
+    space,
+  },
+})
