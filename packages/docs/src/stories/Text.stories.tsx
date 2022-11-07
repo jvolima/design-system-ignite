d8a12d386be55e8e597cda86b700abee6cc1986a
João Vitor Lima <jvolima2004@gmail.com>
2022-11-07 11:46:12 -0300

    Componente Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus tempore vel iste iusto animi quos quidem quam accusamus nemo impedit cumque enim voluptas aspernatur, molestiae ex cum molestias quaerat quis!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
